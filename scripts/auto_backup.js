2fab0232cdd4a0a0364512e85f603b6074c828c9
Kingwcl <1970922141@qq.com>
2024-01-15 18:20:36 +0800

    blog auto backup script's commit

--- scripts/auto_backup.js
+++ scripts/auto_backup.js
@@ -2,30 +2,34 @@ require("shelljs/global");
 try {
   hexo.on("deployAfter", function () {
     //当deploy完成后执行备份
-    run();
+    backup();
   });
 } catch (e) {
   console.log("产生了一个错误啊<(￣3￣)> !，错误详情为：" + e.toString());
 }
-function run() {
+function backup() {
   if (!which("git")) {
-    echo("Sorry, this script requires git");
+    echo("=================Sorry, this script requires git=================");
     exit(1);
   } else {
-    echo("======================Auto Backup Begin===========================");
+    echo(
+      "==============================Auto Backup Begin=============================="
+    );
     cd("D:/blog项目相关/hexoblog"); //此处修改为Hexo根目录路径
     if (exec("git add --all").code !== 0) {
-      echo("Error: Git add failed");
+      echo("=================Error: Git add failed=================");
       exit(1);
     }
     if (exec('git commit -am "blog auto backup script\'s commit"').code !== 0) {
-      echo("Error: Git commit failed");
+      echo("=================Error: Git commit failed=================");
       exit(1);
     }
     if (exec("git push").code !== 0) {
-      echo("Error: Git push failed");
+      echo("=================Error: Git push failed=================");
       exit(1);
     }
-    echo("==================Auto Backup Complete============================");
+    echo(
+      "==============================Auto Backup Complete=============================="
+    );
   }
 }
