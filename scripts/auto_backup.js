f49f4ef41f84fdd48375b97bad8284d1b31f5d20
Kingwcl <1970922141@qq.com>
2024-01-16 10:06:42 +0800

    blog auto backup script's commit

--- scripts/auto_backup.js
+++ scripts/auto_backup.js
@@ -18,6 +18,7 @@ function backup() {
     exit(1);
   } else {
     echo("-e", "${RED_COLOR}======Auto Backup Begin======");
+    echo("-e", "e[31m 红色 e[0m");
     echo(
       "==============================Auto Backup Begin=============================="
     );
